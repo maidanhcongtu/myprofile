3b107a964d273f8af9aa531438aada4eec1485f8
maidanhcongtu <maidanhcongtu@gmail.com>
2016-05-27 18:05:07 +0700

    init data for view

--- app/app.js
+++ app/app.js
@@ -1,16 +1,31 @@
 (function(){
 	
 	"use strict";	
-	var app =angular.module("MyApp",["ngRoute","MyCtrls"])
+	var app =angular.module("MyApp",[
+			"ngRoute",
+			"pascalprecht.translate"
+			"MyCtrls"
+		])
 		.controller("AppCtrl",["$scope",function($scope){
 			
 		}])
 		.config(["$routeProvider",function($routeProvider){
 			$routeProvider
 				.when("/",{
-					templateUrl: "app/home/_home.html",
+					templateUrl: "app/view/home/_home.html",
 					controller: "HomeCtrl"
 				});
-	}]);
-	
+		}])
+		.config(["$translateProvider", function($translateProvider){
+			
+			$translateProvider.useStaticFilesLoader({
+				prefix:"app/asserts/languages/",
+				suffix:".json"
+			})
+			
+			$translateProvider.preferredLanguage("EN");
+			$translateProvider.useLocalStorage();
+			
+		}]);
+		
 })();
